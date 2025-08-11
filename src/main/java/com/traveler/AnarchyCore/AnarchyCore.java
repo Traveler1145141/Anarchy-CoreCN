b75483d12cb15eea8b8511b01dd8e2d4a239bcbd
Traveler114514 <3484181535@qq.com>
2025-08-11 08:17:35 +0800

    Update AnarchyCore.java

--- src/main/java/com/traveler/AnarchyCore/AnarchyCore.java
+++ src/main/java/com/traveler/AnarchyCore/AnarchyCore.java
@@ -44,7 +44,7 @@ import java.util.UUID;
 public class AnarchyCore extends JavaPlugin implements Listener, CommandExecutor {
 
     // 插件版本号
-    public static final String PLUGIN_VERSION = "1.0.0";
+    public static final String PLUGIN_VERSION = "1.2";
     
     private FileConfiguration config;
     private FileConfiguration messages;
